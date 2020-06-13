93a596f4aff3a7c90e7b8f16ce83d1cc6edec331
Ruidong Li <rli012@ucr.edu>
2020-06-13 00:19:13 -0700

    Update Data_Collection.R

--- Circulating_microRNA/Data_Collection.R
+++ Circulating_microRNA/Data_Collection.R
@@ -1,4 +1,11 @@
 
+# 1. Strategy: matching the problem with the solution
+# 2. Exploratory Data Analysis (EDA)
+# 3. Data preparation and preprocessing
+# 4. Feature engineering
+# 5. Model development
+# 6. Conclusion
+
 setwd('~/bigdata/EarlyCancerDetection/miRNA/')
 
 ################################################################
